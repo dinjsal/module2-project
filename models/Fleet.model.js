3ae51bb88c215d2af73021793975da6bd3447083
Doreen <doreenmarie_cee@hotmail.com>
2023-11-07 21:10:48 +0100

    created fleet model

--- models/Fleet.model.js
+++ models/Fleet.model.js
@@ -2,3 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
+const fleetSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    type: {
+        type: String,
+        required: true
+    },
+
+
+    capacity: {
+        type: Number,
+        required: true
+    },  
+
+
+    engine: {
+        type: String,
+        required: true
+    },
+
+    missionProfile: {
+        type: String,
+        required: true
+    },
+
+});
+
+const Fleet = mongoose.model('Fleet', fleetSchema);
+module.exports = Fleet;
