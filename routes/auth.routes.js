ea8ae1f91b41c2780decd9d8a9a39e9251bfd3fe
Doreen <doreenmarie_cee@hotmail.com>
2023-11-07 15:26:51 +0100

    minor changes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,10 +7,6 @@ const User = require("../models/User.model");
 
 // GET routes
 
-// router.get("/register", (req, res, next) => {
-//     res.render("auth/register"); // auth folder, register.hbs
-// });
-
 // only made login.hbs, now it's still empty
 router.get("/login", (req, res, next) => {
     res.render("login"); 
