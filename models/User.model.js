3ac85b9fb64e9d27c2493bdfce4581ffad778f9b
Dinah Salazar <dinahsalazar@gmail.com>
2023-11-08 05:31:02 +0900

    Add style for error message, update auth routes, signup.hbs, considering adding dob to User model

--- models/User.model.js
+++ models/User.model.js
@@ -1,38 +1,36 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    firstName:{
-        type: String,
-        required: [true, 'Please enter your first name']
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please enter your first name"],
     },
 
-    lastName:{
-        type: String,
-        required: [true, 'Please enter your last name']
+    lastName: {
+      type: String,
+      required: [true, "Please enter your last name"],
     },
 
-
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
     },
 
-    password:{
-        type: String,
-        required: [true, 'Please enter a password'],
-        minlength: [2, 'Your password must be at least 8 characters long']
-    }
-}, {
-    timestamps: true
-});
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+      minlength: [2, "Your password must be at least 8 characters long"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,6 @@ const User = require("../models/User.model");
 
 // GET routes
 
-// only made login.hbs, now it's still empty
 router.get("/login", (req, res, next) => {
   res.render("auth/login");
 });
@@ -15,69 +14,7 @@ router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
-// // POST routes
-// router.post("/signup", async (req, res, next) => {
-//   const { firstName, lastName, email, password } = req.body;
-
-//   try {
-//     //check if user exists in the database
-//     const userExists = await User.findOne({ email });
-//     if (userExists) {
-//       res.render("auth/register", { errorMessage: "Email already exists" });
-//       return;
-//     }
-//     //hash password
-//     const salt = bcrypt.genSaltSync(10);
-//     const hashedPassword = bcrypt.hashSync(password, salt);
-
-//     //create new user
-//     const newUser = new User({
-//       firstName,
-//       lastName,
-//       email,
-//       password: hashedPassword,
-//     });
-
-//     //save this new user
-//     await newUser.save();
-
-//     res.send("User successfully registered");
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// //Login POST route, when existing User logs in
-// router.post("/login", async (req, res, next) => {
-//   try {
-//     const user = await User.findOne({ email: req.body.email });
-
-//     if (user) {
-//       const passwordCorrect = await bcrypt.compare(
-//         req.body.password,
-//         user.password
-//       );
-//       if (passwordCorrect) {
-//         req.session.currentUser = user;
-//         res.redirect("/profile");
-//       } else {
-//         res.render("auth/login", {
-//           errorMessage: "Incorrect credentials, please try again",
-//         });
-//       }
-//     }
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// router.get("/profile", (req, res, next) => {
-//   if (req.session.currentUser) {
-//     res.render("profile", { user: req.session.currentUser });
-//   } else {
-//     // Redirect to login if not logged in
-//     res.redirect("/login");
-//   }
-// });
+// POST routes
+router.post("/signup", async (req, res, next) => {});
 
 module.exports = router;
