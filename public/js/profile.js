6150647cae3dfc7619e3333c015269ded08a716c
Doreen <doreenmarie_cee@hotmail.com>
2023-11-11 11:50:39 +0100

    modify logout functionality

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,8 +1,13 @@
 //to handle click event in the vertical navigation bar
-
-
 const makeActive = (element) => {
     document.querySelectorAll('.vertical-menu li').forEach(item => {item.classList.remove('active');
   });
   element.classList.add('active');
   };
+
+
+  //to handle logout event from the navigation bar
+  function performLogout(e){
+    e.preventDefault();
+    document.getElementById('logout').submit();
+  }
