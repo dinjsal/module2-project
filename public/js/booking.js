4f69870f19e4b741ee5dc47dda7b15803e5dc625
Doreen <doreenmarie_cee@hotmail.com>
2023-11-14 14:24:46 +0100

    adding logic and images

--- public/js/booking.js
+++ public/js/booking.js
@@ -1,7 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // to show and hide the destination select options by clicking departure label
 
-    // to initially hide all departure images
+    const departureLabel = document.getElementById('departureLabel');
+    const departureOptions = document.getElementById('departureOptions');
+    const departureSelect = document.getElementById('departureSelect');
+
+    // departureLabel.addEventListener('click', () => {
+    //     if(departureSelect.style.display === 'none' || departureSelect.style.display === '') {
+    //         departureSelect.style.display = 'block';
+    //     } else {
+    //         departureSelect.style.display = 'none';
+    //     }
+    // });
+
+    //converting the departure select options into a list
+    departureLabel.addEventListener('click', () => {
+        departureOptions.style.display = departureOptions.style.display === 'block' ? 'none' : 'block';
+    });
+
+    departureOptions.querySelectorAll('li').forEach(option => {
+        option.addEventListener('click', (event) => {
+            const value = event.target.getAttribute('data-value');
+            departureSelect.value = value;
+            departureOptions.style.display = 'none';
+
+            const eventChange = new Event('change');
+            departureSelect.dispatchEvent(eventChange);
+        });
+
+    });
+
+
+
+    // to initially hide all departure images but show the default image
     document.querySelectorAll('.images-container img').forEach(img => img.style.display = 'none');
+
+    document.getElementById('earth').style.display = 'block';
+
+    document.getElementById('flightlogo').style.display = 'block';
 
     document.getElementById('departureSelect').addEventListener('change', event => {
         updateImageDisplay(event.target.value, 'departure-section');
@@ -13,8 +49,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const updateImageDisplay = (selection, section) => {
 
-       //to hide the images from this section
+       //to hide the images from this section and show default image if the selection is empty
         document.querySelectorAll(`.${section} .images-container img`).forEach(img => img.style.display = 'none');
+
+        if(!selection) {
+            if (section === 'departure-section') {
+                document.getElementById('earth').style.display = 'block';
+            } else if (section === 'destination-section') {
+                document.getElementById('flightlogo').style.display = 'block';
+            }
+            return;
+            
+        }
 
 
         // to determine which image to show
