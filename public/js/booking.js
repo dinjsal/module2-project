b910c66de5dc694919da34d7fe6fb7b678c7d9ad
Doreen <doreenmarie_cee@hotmail.com>
2023-11-14 10:48:36 +0100

    modify logic

--- public/js/booking.js
+++ public/js/booking.js
@@ -1,27 +1,41 @@
-//to show images of the selected country and destination
-document.getElementById('departureSelect').addEventListener('change', () => {
-    showCountryImage(this.value, 'departure');
+document.addEventListener('DOMContentLoaded', () => {
+
+    // to initially hide all departure images
+    document.querySelectorAll('.images-container img').forEach(img => img.style.display = 'none');
+
+    document.getElementById('departureSelect').addEventListener('change', event => {
+        updateImageDisplay(event.target.value, 'departure-section');
+    });
+
+    document.getElementById('destinationSelect').addEventListener('change', event => {
+        updateImageDisplay(event.target.value, 'destination-section');
+    });
+
+    const updateImageDisplay = (selection, section) => {
+
+       //to hide the images from this section
+        document.querySelectorAll(`.${section} .images-container img`).forEach(img => img.style.display = 'none');
+
+
+        // to determine which image to show
+        let imageId;
+        if (section === 'departure-section') {
+            imageId = selection === 'Texas, USA' ? 'depUsa' : 'depSpain';
+        } else {
+            imageId = selection.toLowerCase();
+        }
+
+        // to show selected image
+        const imageElement = document.getElementById(imageId);
+        if (imageElement) {
+            imageElement.style.display = 'block';
+        }
+    }
 });
-
-
-document.getElementById('destinationSelect').addEventListener('change', () => {
-    showCountryImage(this.value, 'destination');
-});
-
-
-function showCountryImage(country, type) {
-    // to hide all images
-    document.querySelectorAll('.' + type + '-section .images-container img').forEach(img => {img.style.display = 'none';
-});
-
-    // to show the selected country image
-    const imgId = type + country;
-    document.getElementById(imgId).style.display = 'block';
-
-}
 
 
 //to submit the booking form
 document.getElementById('submitBtn').addEventListener('click', function () {
     document.getElementById('bookingForm').submit();
 });
+
