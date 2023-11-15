d2a05f8899b1c56f05d0a62512c6b0a1bfed2796
Dinah Salazar <dinahsalazar@gmail.com>
2023-11-16 03:34:31 +0900

    Add loggedInUser to profile.hbs

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,12 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+// authenticated user already logged in, able to see the profile page
+// no need/can't access /auth/login
 router.get("/profile", isLoggedIn, (req, res, next) => {
   const loggedInUser = req.session.currentUser;
   res.render("auth/profile", { loggedInUser });
+  // console.log(loggedInUser);
 });
 
 module.exports = router;
