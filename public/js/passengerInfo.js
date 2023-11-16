49f41906059946e6ab70a03ce292374390ec1e69
Doreen <doreenmarie_cee@hotmail.com>
2023-11-16 20:09:06 +0100

    completing passenger-info

--- public/js/passengerInfo.js
+++ public/js/passengerInfo.js
@@ -1,16 +1,63 @@
-let counter = 0;
 
-    document.getElementById('plus').addEventListener('click', function() {
-        counter++;
-        document.getElementById('passengerNum').textContent = counter;
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  let counter = 1; 
 
-    document.getElementById('minus').addEventListener('click', function() {
-        if (counter > 0) {
-            counter--;
-            document.getElementById('passengerNum').textContent = counter;
-        }
-    });
+  // increment button event listener
+  document.getElementById('plus').addEventListener('click', function() {
+      if (counter < 2) {
+          counter++;
+          updateFormDisplay(counter);
+      }
+  });
+
+  // decrement button event listener
+  document.getElementById('minus').addEventListener('click', function() {
+      if (counter > 1) {
+          counter--;
+          updateFormDisplay(counter);
+      }
+  });
+
+  //  to update the form display and apply styles
+  function updateFormDisplay(counterValue) {
+      document.getElementById('passengerNum').textContent = counterValue;
+
+      if (counterValue === 2) {
+          document.getElementById('form2').style.display = 'block';
+          applyStylesToForms();
+      } else {
+          document.getElementById('form2').style.display = 'none';
+          resetStylesForForm1();
+      }
+  }
+
+  //  to apply styles to both forms
+  function applyStylesToForms() {
+      let forms = document.querySelectorAll('.form1, .form2');
+      forms.forEach(function(form) {
+          form.style.paddingTop = '5%';
+          form.style.marginLeft = '5%';
+          form.style.marginBottom = '5%';
+          form.style.marginRight = '10%';
+      });
+  }
+
+  //  to reset styles for form1
+  function resetStylesForForm1() {
+      let form1 = document.querySelector('.form1');
+      form1.style.cssText = ''; 
+      form1.style.marginLeft = '25%';
+      form1.style.marginRight = '25%';
+  }
+});
+
+
+
+
+
+
+
+  
 
 
 
