08127c055c0491ee7c2b318f8b504996b64a13ee
Doreen <doreenmarie_cee@hotmail.com>
2023-11-18 19:08:54 +0100

    completing our fleet page

---
+++ public/js/ourFleet.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.fleet2, .fleet3, .fleet4, .fleet5').forEach((fleet) => {
+    const fleetinformation = fleet.querySelector('.info-fleet2, .info-fleet3, .info-fleet4, .info-fleet5');
+
+
+    
+});
