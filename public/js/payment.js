a035fa7164d3a6794fa0865493d90baee02252ff
Dinah Salazar <dinahsalazar@gmail.com>
2023-11-18 19:52:54 +0900

    Update payment.hbs style.css js files

--- public/js/payment.js
+++ public/js/payment.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const completePayment = document.querySelector(".btn");
+  const completePayment = document.querySelector(".completePayment");
   const cardName = document.querySelector(".cardName");
   const cardNameOutput = document.querySelector(".cardNameOutput");
   const cardNumber = document.querySelector(".cardNumber");
@@ -9,6 +9,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const paymentInfoDiv = document.querySelector(".payment-info");
   const paymentDisplay = document.querySelector(".payment-display");
   const confirmPayment = document.querySelector(".confirmPayment");
+  const bitcoinPayment = document.querySelector(".bitcoin");
+  const bitcoinDisplay = document.querySelector(".payment-bitcoin");
+  const completePaymentBit = document.querySelector(".completePaymentBit");
+  const bitcoinOutput = document.querySelector(".bitcoin-output");
+  const bitcoinNum = document.querySelector(".bitcoinNum");
+  const paymentBitcoin2 = document.querySelector(".payment-bitcoin2");
+  const confirmPaymentBit = document.querySelector(".confirmPaymentBit");
 
   function capturePaymentInfo() {
     cardNameOutput.innerHTML = cardName.value;
@@ -25,6 +32,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  function payWithBitcoin() {
+    if (bitcoinDisplay.style.display === "none") {
+      bitcoinDisplay.style.display = "block";
+      paymentDisplay.style.display = "none";
+      paymentInfoDiv.style.display = "none";
+      completePayment.style.display = "none";
+      confirmPayment.style.display = "none";
+      completePaymentBit.style.display = "block";
+    }
+  }
+
+  function capturePaymentBitcoin() {
+    if (paymentBitcoin2.style.display === "none") {
+      paymentBitcoin2.style.display = "block";
+      bitcoinOutput.innerHTML = bitcoinNum.value;
+      bitcoinDisplay.style.display = "none";
+      completePaymentBit.style.display = "none";
+      confirmPaymentBit.style.display = "block";
+    }
+  }
+
   completePayment.addEventListener("click", capturePaymentInfo);
   completePayment.addEventListener("click", displayPaymentInfo);
+  bitcoinPayment.addEventListener("click", payWithBitcoin);
+  completePaymentBit.addEventListener("click", capturePaymentBitcoin);
 });
