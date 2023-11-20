b797cee9bbf0920f29a2e7c9af64ec45bb864274
Dinah Salazar <dinahsalazar@gmail.com>
2023-11-20 20:15:11 +0900

    Update crew

--- public/js/crew.js
+++ public/js/crew.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const pilot1ReadMore = document.querySelector(".pilot1");
+  const demo = document.querySelector("#demo");
   const description1 = document.querySelector(".description1");
 
   function showPilot1() {
     if (description1.style.display === "none") {
-      description1.style.display = "block";
+      description1.style.display === "block";
+      demo.style.display = "none";
     }
   }
   pilot1ReadMore.addEventListener("click", showPilot1);
